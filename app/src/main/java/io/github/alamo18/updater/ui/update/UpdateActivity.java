8dec77be646896c5b3ee9b583104c011272da16b
Morgan Lee <morganleezd2003@googlemail.com>
2017-08-29 00:47:32 -0700

    Main: move and rename class

--- app/src/main/java/io/github/alamo18/updater/ui/update/UpdateActivity.java
+++ app/src/main/java/io/github/alamo18/updater/ui/update/UpdateActivity.java
@@ -1,4 +1,4 @@
-package io.github.alamo18.updater;
+package io.github.alamo18.updater.ui.update;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -13,13 +13,15 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity
+import io.github.alamo18.updater.R;
+
+public class UpdateActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_update);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
