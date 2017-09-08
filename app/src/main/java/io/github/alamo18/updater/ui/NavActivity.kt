e2f42871aa52f340009495d1641ce6fde5c5e159
Morgan Lee <morganleezd2003@googlemail.com>
2017-09-08 10:19:55 -0700

    Add menu items and handlers

--- app/src/main/java/io/github/alamo18/updater/ui/NavActivity.kt
+++ app/src/main/java/io/github/alamo18/updater/ui/NavActivity.kt
@@ -5,11 +5,15 @@ import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
 import android.support.v4.app.Fragment
 import android.support.v7.widget.Toolbar
+import android.view.Menu
 import android.view.MenuItem
 import io.github.alamo18.updater.R
 import io.github.alamo18.updater.ui.fragment.InstalledFragment
 import io.github.alamo18.updater.ui.fragment.PackageListFragment
 import io.github.alamo18.updater.ui.fragment.StoreFragment
+import android.view.MenuInflater
+
+
 
 class NavActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSelectedListener {
 
@@ -53,5 +57,42 @@ class NavActivity : AppCompatActivity(), BottomNavigationView.OnNavigationItemSe
         supportFragmentManager.beginTransaction()
                 .replace(R.id.container, fragment, "content")
                 .commit()
+    }
+
+    // Inflate main menu
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Menu item handlers
+        when (item.itemId) {
+            R.id.action_search ->
+                //morph the search
+                return true
+
+            R.id.menu_help -> {
+                showHelp()
+                return true
+            }
+
+            R.id.menu_change_device -> return true
+
+            R.id.menu_settings ->
+                //launch settings
+                return true
+
+            R.id.menu_about ->
+                // launch aboutActivity
+                return true
+
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showHelp() {
+        // intent to startActivity(helpActivity)
     }
 }
