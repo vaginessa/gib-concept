3ef0709e618d9bedcb4bba9b9dcdd244831da8fe
Morgan Lee <morganleezd2003@googlemail.com>
2017-09-11 03:32:59 -0700

    [utils] RequestInterface: Migrate json static url

--- app/src/main/java/io/github/alamo18/updater/utils/RequestInterface.kt
+++ app/src/main/java/io/github/alamo18/updater/utils/RequestInterface.kt
@@ -5,6 +5,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface RequestInterface {
-    @get:GET("/alamo18/3222f298b315e39df55afe8b0154a650/raw/fb0035d59aadc29bfc718c6290f8d529b1eb63a9/gistfile1.txt")
+    @get:GET("/alamo18/gib/master/repo_data/repo.json")
     val json: Call<JSONResponse>
 }
